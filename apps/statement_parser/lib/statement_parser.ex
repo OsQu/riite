174b38ce16671a8be43b2d8650f66a9bab7edbb5
Oskari Virtanen <oskari@flowdock.com>
2017-04-16 19:19:18 +0300

    Scaffold for statement_parser

---
+++ apps/statement_parser/lib/statement_parser.ex
@@ -0,0 +1,5 @@
+defmodule StatementParser do
+  def main(_) do
+    IO.puts "Hello world"
+  end
+end
---
+++ apps/statement_parser/test/statement_parser_test.exs
@@ -0,0 +1,8 @@
+defmodule StatementParserTest do
+  use ExUnit.Case
+  doctest StatementParser
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
