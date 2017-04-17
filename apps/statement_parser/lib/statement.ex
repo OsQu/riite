658e5c67da6f800ad536a64f6981d01067acdb17
Oskari Virtanen <oskari@flowdock.com>
2017-04-17 19:26:17 +0300

    Implement String.Chars for statement

--- apps/statement_parser/lib/statement.ex
+++ apps/statement_parser/lib/statement.ex
@@ -1,4 +1,10 @@
 defmodule Statement do
   use Timex
   defstruct date: DateTime.to_date(Timex.now), amount: 0, to_from: ""
+
+  defimpl String.Chars, for: Statement do
+    def to_string(statement) do
+      "#{statement.date}\t#{statement.to_from}\t#{statement.amount}"
+    end
+  end
 end
